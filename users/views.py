b81fb3561f3154f118dbdf09b7883cf564c3e54f
Jakub <jakub.kos212@gmail.com>
2024-01-09 18:06:59 +0100

    add register logic

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.http import HttpResponse, HttpResponseForbidden
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 def login_user(request):
     if request.method == "POST":
@@ -23,6 +25,25 @@ def logout_user(request):
     logout(request)
 
     return redirect('/')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registration Successful!")
+            return redirect('/')
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'auth/register.html', {
+        'form': form,
+    })
 
 
 def register_redirect(request):
