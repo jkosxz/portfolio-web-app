fb5076560fb7652bc51c275b287290d0f76d8c27
zamalos <jakub.kos212@gmail.com>
2024-01-13 18:09:59 +0100

    adding new logics around user handling

--- users/views.py
+++ users/views.py
@@ -5,17 +5,20 @@ from django.http import HttpResponse, HttpResponseForbidden
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterUserForm
 
+
 def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        print("User logging in")
+        try:
+            print("user is not none")
             login(request, user)
             return redirect('index')
-        else:
-            messages.success(request, "There Was An Error Logging In, Try Again...")
-            return redirect('login')
+        except:
+            messages.add_message(request, messages.ERROR, 'Error logging in')
+            return render(request, "auth/login.html")
 
     else:
         return render(request, 'auth/login.html', {})
