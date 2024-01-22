dd5c2ca4d7f9b8d823d484120c180eca01e52f6a
Jakub <jakub.kos212@gmail.com>
2024-01-22 18:47:44 +0100

    add succes message after logging in

--- users/views.py
+++ users/views.py
@@ -13,6 +13,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         try:
             login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'You\'ve logged in')
             return redirect('index')
         except:
             messages.add_message(request, messages.ERROR, 'Error logging in')
